ee1299ab0b8d13f5e0ba1d699a64d78293ee1de6
David Chapdelaine <david_chapdelaine@hotmail.com>
2017-03-20 17:43:21 -0400

    Added a test project to store a pregenerated sentiment analysis output to document db

---
+++ 2017/Step5DocumentDBConsole/AnalyzedMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentDBTestConsole
+{
+    public class AnalyzedMessage
+    {
+        public string userId { get; set; }
+
+        public string message { get; set; }
+
+        public double sentimentScore { get; set; }
+    }
+}
---
+++ 2017/Step5DocumentDBConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Step5DocumentDBConsole")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5271a9d-c6e2-4da8-bbb0-c7532ca2930a")]
