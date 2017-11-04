be4db0c02ae095195d5e9c1b636affa7d8b192c7
Hinault <ngdonfack@gmail.com>
2017-11-04 14:39:38 -0400

    added code snippets #

--- 2017/Step 2 - Entity Framework/Code snippets/Statistic.cs
+++ 2017/Step 2 - Entity Framework/Code snippets/Statistic.cs
@@ -1,9 +1,11 @@
-using System;
+﻿using System;
 
 namespace WebAppAspNetCore.Models
 {
     public class Statistic
     {
+        public int Id { get; set; }
+
         public int BestTime { get; set; }
 
         public int AverageTime { get; set; }
